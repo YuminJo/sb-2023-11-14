2fda27b6b304ae7cb2b885e44556d2684f0d8e9a
YuminJo <nationmilo@gmail.com>
2023-11-16 09:49:57 +0900

    server case catch

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -3,11 +3,11 @@ package com.ll.sb20231114.domain.article.article.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rq.Rq;
@@ -16,7 +16,6 @@ import com.ll.sb20231114.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,6 +34,19 @@ public class ArticleController {
 		String title,
 		String body
 	) {
+		if ( title == null || title.trim().isEmpty()) {
+			return new RsData<>(
+				"F-1",
+				"제목을 입력해주세요."
+			);
+		}
+
+		if ( body == null || body.trim().isEmpty()) {
+			return new RsData<>(
+				"F-2",
+				"내용을 입력해주세요."
+			);
+		}
 		Article article = articleService.write(title, body);
 
 		RsData<Article> rs = new RsData<>(
@@ -46,28 +58,28 @@ public class ArticleController {
 		return rs;
 	}
 
-	//전통적인 Servlet 방식
-	@PostMapping("/article/write2")
-	@ResponseBody
-	@SneakyThrows
-	void write2(
-		HttpServletRequest req,
-		HttpServletResponse resp
-	) {
-		String title = req.getParameter("title");
-		String body = req.getParameter("body");
-		Article article = articleService.write(title, body);
-
-		RsData<Article> rs = new RsData<>(
-			"S-1",
-			"%d번 게시물이 작성되었습니다.".formatted(article.getId()),
-			article
-		);
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		resp.setCharacterEncoding("UTF-8");
-		resp.getWriter().println(objectMapper.writeValueAsString(rs));
-	}
+	// //전통적인 Servlet 방식
+	// @PostMapping("/article/write2")
+	// @ResponseBody
+	// @SneakyThrows
+	// void write2(
+	// 	HttpServletRequest req,
+	// 	HttpServletResponse resp
+	// ) {
+	// 	String title = req.getParameter("title");
+	// 	String body = req.getParameter("body");
+	// 	Article article = articleService.write(title, body);
+	//
+	// 	RsData<Article> rs = new RsData<>(
+	// 		"S-1",
+	// 		"%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+	// 		article
+	// 	);
+	//
+	// 	ObjectMapper objectMapper = new ObjectMapper();
+	// 	resp.setCharacterEncoding("UTF-8");
+	// 	resp.getWriter().println(objectMapper.writeValueAsString(rs));
+	// }
 
 	@GetMapping("/article/getLastArticle")
 	@ResponseBody
@@ -109,5 +121,13 @@ public class ArticleController {
 	String rqPointer()
 	{
 		return rq.toString();
+	}
+
+	@GetMapping("/article/rqTest")
+	String showRqTest(Model model) {
+		String rqToString = rq.toString();
+
+		model.addAttribute("rqToString", rqToString);
+		return "article/rqTest";
 	}
 }
