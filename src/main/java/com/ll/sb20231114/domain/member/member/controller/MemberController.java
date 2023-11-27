2a65003277ad05e8c55da44af67cb574a7b0fb25
YuminJo <nationmilo@gmail.com>
2023-11-27 15:11:32 +0900

    ArticleController Valid Write2

--- src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
@@ -1,15 +1,17 @@
 package com.ll.sb20231114.domain.member.member.controller;
 
-import com.ll.sb20231114.domain.member.member.service.MemberService;
-import com.ll.sb20231114.global.rq.Rq;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ll.sb20231114.domain.member.member.service.MemberService;
+import com.ll.sb20231114.global.rq.Rq;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
