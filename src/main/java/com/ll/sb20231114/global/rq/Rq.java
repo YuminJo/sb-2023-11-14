45506092153d6137bbaee7e15b98f0b00600e11b
YuminJo <nationmilo@gmail.com>
2023-11-27 10:25:22 +0900

    base Layout

--- src/main/java/com/ll/sb20231114/global/rq/Rq.java
+++ src/main/java/com/ll/sb20231114/global/rq/Rq.java
@@ -1,20 +1,22 @@
 package com.ll.sb20231114.global.rq;
 
-import com.ll.sb20231114.domain.member.member.entity.Member;
-import com.ll.sb20231114.domain.member.member.service.MemberService;
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import com.ll.sb20231114.domain.member.member.entity.Member;
+import com.ll.sb20231114.domain.member.member.service.MemberService;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @RequestScope
 @Component
@@ -75,6 +77,8 @@ public class Rq {
     }
 
     public boolean isAdmin() {
+        if (!isLogined()) return false;
+
         return user.getAuthorities()
                 .stream()
                 .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
