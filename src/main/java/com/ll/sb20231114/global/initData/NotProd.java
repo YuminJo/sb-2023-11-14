b8ecbd08ccf219837ba8e7b0274d36a3ebe21896
YuminJo <nationmilo@gmail.com>
2023-11-27 16:52:01 +0900

    RsData

--- src/main/java/com/ll/sb20231114/global/initData/NotProd.java
+++ src/main/java/com/ll/sb20231114/global/initData/NotProd.java
@@ -1,11 +1,12 @@
 package com.ll.sb20231114.global.initData;
 
-import com.ll.sb20231114.domain.article.article.service.ArticleService;
-import com.ll.sb20231114.domain.member.member.entity.Member;
-import com.ll.sb20231114.domain.member.member.service.MemberService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.ll.sb20231114.domain.article.article.service.ArticleService;
+import com.ll.sb20231114.domain.member.member.entity.Member;
+import com.ll.sb20231114.domain.member.member.service.MemberService;
 
 @Configuration
 public class NotProd {
@@ -15,9 +16,9 @@ public class NotProd {
             ArticleService articleService
     ) {
         return args -> {
-            Member memberAdmin = memberService.join("admin", "1234");
-            Member memberUser1 = memberService.join("user1", "1234");
-            Member memberUser2 = memberService.join("user2", "1234");
+            Member memberAdmin = memberService.join("admin", "1234").getData();
+            Member memberUser1 = memberService.join("user1", "1234").getData();
+            Member memberUser2 = memberService.join("user2", "1234").getData();
 
             articleService.write(memberAdmin, "제목1", "내용1");
             articleService.write(memberUser1, "제목2", "내용2");
